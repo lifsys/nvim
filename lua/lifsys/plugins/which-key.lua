f59f2296856a7d2d5e4959653606e10570803648
Lifsys <23331007+lifsys@users.noreply.github.com>
2024-01-07 10:40:25 -0700

    update

--- lua/lifsys/plugins/which-key.lua
+++ lua/lifsys/plugins/which-key.lua
@@ -12,7 +12,7 @@ return {
       ["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
       ["<leader>h"] = { name = "Git [H]unk", _ = "which_key_ignore" },
       ["<C-\\>"] = { "<cmd>NvimTreeToggle<cr>", "Explorer" },
-      ["<S-t>"] = { "<cmd>lua require('nvterm.terminal').toggle('float')<cr>", "Floating Terminal" },
+      ["<C-S-T>"] = { "<cmd>lua require('nvterm.terminal').toggle('float')<cr>", "Floating Terminal" },
       ["<C-S-H>"] = { "<cmd>lua require('nvterm.terminal').toggle('horizontal')<cr>", "Horizontal Terminal" },
       ["<C-S-V>"] = { "<cmd>lua require('nvterm.terminal').toggle('vertical')<cr>", "Vertical Terminal" },
       ["<C-?>"] = { "<cmd>ChatGPT<cr>", "ChatGPT" },
